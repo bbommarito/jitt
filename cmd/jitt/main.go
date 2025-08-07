cacce9658eeb8bb4abe0e0c983d81f006ac0a4a2
Brian "Burrito" Bommarito <6898085+bbommarito@users.noreply.github.com>
2025-08-07 14:18:09 -0400

    Hooking up the jira namespace.

--- cmd/jitt/main.go
+++ cmd/jitt/main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"github.com/bbommarito/jitt/internal/gitpassthrough"
+	"github.com/bbommarito/jitt/internal/jira"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	args := os.Args[1:]
+	if len(args) < 1 {
 		os.Exit(1)
 	}
 
-	args := os.Args[1:]
-
-	err := gitpassthrough.RunGit(args)
-	if err != nil {
-		os.Exit(1)
+	switch args[0] {
+	case "jira":
+		jira.Handle(args[1:])
+	default:
+		err := gitpassthrough.RunGit(args)
+		if err != nil {
+			os.Exit(1)
+		}
 	}
 }
